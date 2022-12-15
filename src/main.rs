13c4b55be7bbb989528cdf3d243064a27c51c6a9
Sam Collier <sam@samcollier.tech>
2022-12-15 16:05:48 +0000

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,9 @@ mod commands;
 use std::env;
 
 use serenity::async_trait;
-use serenity::builder::CreateInteractionResponseFollowup;
-//use serenity::model::application::command::Command;
 use serenity::model::application::interaction::{Interaction, InteractionResponseType};
 use serenity::model::gateway::Ready;
 use serenity::model::id::GuildId;
-use serenity::model::prelude::ChannelId;
-use serenity::model::prelude::application_command::ApplicationCommandInteraction;
 use serenity::prelude::*;
 
 struct Handler;
@@ -24,8 +20,6 @@ impl EventHandler for Handler
 
 			let content = match command.data.name.as_str()
 			{
-				"ping" => commands::ping::run(&command.data.options),
-				"numberinput" => commands::numberinput::run(&command.data.options),
 				"update-cookie" => commands::update_cookie::run(&command.data.options),
 				"update-authentication" => commands::update_authentication::run(&command.data.options),
 				"gpt" =>
@@ -41,21 +35,8 @@ impl EventHandler for Handler
 					typing.stop().unwrap();
 					"".to_string()
 				},
-				//"id" => commands::id::run(&command.data.options),
-				//"attachmentinput" => commands::attachmentinput::run(&command.data.options),
 				_ => "not implemented :(".to_string(),
 			};
-
-			if let Err(why) = command
-				.create_interaction_response(&ctx.http, |response| {
-					response
-						.kind(InteractionResponseType::ChannelMessageWithSource)
-						.interaction_response_data(|message| message.content(content))
-				})
-				.await
-			{
-				//println!("Cannot respond to slash command: {}", why);
-			}
 		}
 	}
 
@@ -71,32 +52,14 @@ impl EventHandler for Handler
 				.expect("GUILD_ID must be an integer"),
 		);
 
-		let commands = GuildId::set_application_commands(&guild_id, &ctx.http, |commands|
+		GuildId::set_application_commands(&guild_id, &ctx.http, |commands|
 		{
 			commands
-				.create_application_command(|command| commands::ping::register(command))
-				//.create_application_command(|command| commands::id::register(command))
-				//.create_application_command(|command| commands::welcome::register(command))
-				.create_application_command(|command| commands::numberinput::register(command))
-				//.create_application_command(|command| commands::attachmentinput::register(command))
 				.create_application_command(|command| commands::update_cookie::register(command))
 				.create_application_command(|command| commands::update_authentication::register(command))
 				.create_application_command(|command| commands::gpt::register(command))
 		})
-		.await;
-
-		match commands
-		{
-			Ok(commands) =>
-			{
-				//println!("Slash commands registered: {:#?}", commands);
-			}
-			Err(why) =>
-			{
-				//println!("Cannot register slash commands: {}", why);
-			}
-		}
-
+		.await.unwrap();
 	}
 }
 
@@ -112,13 +75,7 @@ async fn main()
 		.await
 		.expect("Error creating client");
 
-	// Finally, start a single shard, and start listening to events.
-	//
-	// Shards will automatically attempt to reconnect, and will perform
-	// exponential backoff until it reconnects.
-	if let Err(why) = client.start().await
-	{
-		//println!("Client error: {:?}", why);
-	}
+	// Start client
+	client.start().await.unwrap();
 }
 
